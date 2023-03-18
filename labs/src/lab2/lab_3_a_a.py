7999941882057e46951c6cb12245953ccc8c9d06
julia <juliafarganus@gmail.com>
2023-03-18 08:24:35 +0100

    Working now

--- labs/src/lab2/lab_3_a_a.py
+++ labs/src/lab2/lab_3_a_a.py
@@ -1,4 +1,4 @@
-from labs.src.lab2.lab_2_a import count_logs_with_response_code
+from labs.src.lab2.lab_3_a import count_logs_with_response_code
 
 
 if __name__ == '__main__':
--- labs/src/lab2/lab_3_a_c.py
+++ labs/src/lab2/lab_3_a_c.py
@@ -1,4 +1,6 @@
-from labs.src.lab2.lab_2_a import count_logs_with_response_code
+from labs.src.lab2.lab_3_a import count_logs_with_response_code
+
+
 
 if __name__ == '__main__':
     print(count_logs_with_response_code(404))
