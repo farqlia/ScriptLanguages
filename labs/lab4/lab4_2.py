75172d8055fbcd92e8625544b5cbfa18c565b9c1
julia <juliafarganus@gmail.com>
2023-03-31 23:05:56 +0200

    find executable files

---
+++ labs/lab4/lab4_2.py
@@ -0,0 +1,45 @@
+import os
+import pathlib
+import sys
+
+
+def get_path_directories():
+    path = os.environ['PATH']
+    return list(filter(lambda s: len(s.strip()) > 0, path.split(os.pathsep)))
+
+
+def is_exe_windows(filepath):
+    return filepath.endswith(".exe")
+
+
+def is_exe_posix(filepath):
+    os.access(filepath, os.X_OK)
+
+
+def is_executable():
+    print("invoked")
+    return is_exe_windows if os.name == 'nt' else is_exe_posix
+
+
+def get_path_contents(include_execs=False):
+    dirs = get_path_directories()
+    if include_execs:
+        is_exe = is_executable()
+        dirs = {_dir: list(filter(is_exe, os.listdir(_dir))) for _dir in dirs if pathlib.Path(_dir).is_dir()}
+    return dirs
+
+
+def print_exe():
+    return len(sys.argv) > 1 and sys.argv[1].lower().startswith('t')
+
+
+if __name__ == "__main__":
+
+    if print_exe():
+        for k, v in get_path_contents(True).items():
+            print(f"[d] {k}")
+            print(f"{v}")
+
+    else:
+        for d in get_path_contents(False):
+            print(f"{d}")
--- labs/lab4/test/test_lab_4.py
+++ labs/lab4/test/test_lab_4.py
@@ -1,10 +1,48 @@
 import os
+import sys
+from pathlib import Path
+import pytest_mock
+
+from labs.lab4.lab4_2 import get_path_contents
 
 
 class TestPrintExecutable:
 
+    def test_platform(self):
+        # for unix, macos it will be posix
+        assert os.name == 'nt'
+
     def test_print_os_separator(self):
         print(os.pathsep)
 
-    # def test_print_PATH_folders(self):
-      #   print(get_path_contents(include_execs=False))
+    def test_if_executable(self):
+        not_executable = Path(r"C:\Users\julia\anaconda3\bin\nvcc.profile")
+        executable = Path(r"C:\Users\julia\anaconda3\bin\nvlink.exe")
+
+        assert not os.access(not_executable, os.X_OK)
+        assert os.access(executable, os.X_OK)
+
+    def test_get_path_folders_and_execs(self):
+        dirs_and_execs = get_path_contents(include_execs=True)
+
+        assert dirs_and_execs["C:\\Users\\julia\\anaconda3\\envs\\ScriptLanguages"] == ['python.exe', 'pythonw.exe']
+
+    def test_get_path_folders(self):
+        dirs = get_path_contents(False)
+        for dir in dirs:
+            print(dir)
+
+    def test_mock_print_path_folders_and_files(self, mocker):
+        mocker.patch('labs.lab4.lab4_2.is_executable',
+                          return_value=True)
+        print(get_path_contents(include_execs=True))
+
+    def test_mock_print_path_folders_and_execs(self, mocker):
+        mocker.patch('labs.lab4.lab4_2.get_path_directories',
+                          return_value=[r"C:\Users\julia\PycharmProjects\ScriptLanguages\labs"])
+        print(get_path_contents(include_execs=True))
+
+    def test_mock_print_path_folders(self, mocker):
+        mocker.patch('labs.lab4.lab4_2.get_path_directories',
+                           return_value=[r"C:\Users\julia\PycharmProjects\ScriptLanguages\labs\lab4"])
+        print(get_path_contents(include_execs=False))
